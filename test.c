7d55d94a2053893e60ee27240bd0707187b346bc
vythili6 <yo.vythilingum@mail.utoronto.ca>
2025-05-07 21:57:59 -0400

    implemented a test for the webserver

---
+++ test.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include "Server.h"
+#include <string.h>
+
+void launch(TheServer *server){
+    char buffer[3000];
+    printf("Waiting for connections...\n");
+    int address_length = sizeof(server->address);
+    int new_socket = accept(server->socket, (struct sockaddr *)&server->address, (socklen_t *)&address_length);
+    read(new_socket, buffer, 3000);
+    printf("%s\n", buffer);
+    char *response = "HTTP/1.1 200 OK\n\nHello World";
+    write(new_socket, response, strlen(response));
+    close(new_socket);
+
+}
+
+int main(){
+    TheServer server = createServer(AF_INET, SOCK_STREAM, 0, INADDR_ANY, 80, 10, launch);
+    server.launch(&server);
+    return 0;
+}
